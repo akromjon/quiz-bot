bc574d0496a779ac64ee003dcc58f82fbac1c3f2
akromjon <akromjon>
2024-09-14 12:49:24 +0000

    feature: Widgets

---
+++ app/Filament/Pages/Dashboard.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use App\Filament\Resources\TelegramUserResource\Widgets\TelegramUserTableWidget;
+use App\Filament\Widgets\QuizOverview;
+use App\Filament\Widgets\TelegramUserStatsOverview;
+use Filament\Pages\Dashboard as PagesDashboard;
+
+class Dashboard extends PagesDashboard
+{
+
+    public function getWidgets(): array
+    {
+        return [
+            QuizOverview::class,
+            TelegramUserStatsOverview::class,
+            // TelegramUserTableWidget::class,
+        ];
+    }
+
+}
