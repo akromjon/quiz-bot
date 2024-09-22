ce357965c52c0acaa7c376b55be64372e5dbc205
akromjon <akromjon>
2024-09-22 19:01:55 +0000

    feature: fix

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('app:check-user-tariff-command')->hourly();
-Schedule::command('backup:sqlite')->environments('production')->hourly();
+Schedule::command('backup:sqlite')->environments(['production'])->hourly();
 
